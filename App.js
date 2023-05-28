f41588063f248154cf59db0b17145ac6f3175b13
LudwigNordfeldt <kostiantyn.fedosieiev@stud.onu.edu.ua>
2023-05-28 08:59:22 +0300

    improved query handling

--- App.js
+++ App.js
@@ -2,9 +2,9 @@ const express = require("express");
 const axios = require("axios");
 
 const app = express();
-const baseUrl = "https://openlibrary.org/"
+const baseUrl = "https://openlibrary.org/";
 
-app.use(express.static("build"))
+app.use(express.static("build"));
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
@@ -12,30 +12,35 @@ app.use((req, res, next) => {
 });
 
 app.get("/books/:request/:subrequest", async (req, res) => {
-  const query = req.query
-  const reqType = req.params.request.concat("/", req.params.subrequest, ".json")
-  let queryUrl = "?"
+  const query = req.query;
+  const reqType = req.params.request.concat(
+    "/",
+    req.params.subrequest,
+    ".json"
+  );
+  let queryUrl = "?";
 
   for (const [key, value] of Object.entries(query)) {
-    queryUrl = queryUrl.concat(key, "=", value, "&")
+    queryUrl = queryUrl.concat(key, "=", value, "&");
   }
 
-  const response = await axios.get(baseUrl.concat(reqType, queryUrl))
-  res.send(response.data)
+  const response = await axios.get(baseUrl.concat(reqType, queryUrl));
+  res.send(response.data);
 });
 
-app.get("/books/:request", async (req, res) => {
-  const query = req.query
-  const reqType = req.params.request.concat(".json")
-  let queryUrl = "?"
+app.get("/books/:type", async (req, res) => {
+  const query = req.query;
+  const reqType = req.params.type.concat(".json");
+  let queryUrl = "?";
 
   for (const [key, value] of Object.entries(query)) {
-    queryUrl = queryUrl.concat(key, "=", value, "&")
+    if (value.length) {
+      queryUrl = queryUrl.concat(key, "=", value, "&");
+    }
   }
 
-  console.log("REQUEST GOING TO:", baseUrl.concat(reqType, queryUrl))
-  const response = await axios.get(baseUrl.concat(reqType, queryUrl))
-  res.send(response.data)
-})
+  const response = await axios.get(baseUrl.concat(reqType, queryUrl));
+  res.send(response.data);
+});
 
 module.exports = app;
